8b75ed92bdaff6806f9be1be5f434c5f676ca731
Taras Pushkar <pushkint@gmail.com>
2024-04-30 16:23:33 +0300

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Taras Pushkar on 30.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
